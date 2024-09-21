a775bad7ed2671b39fcc9a476d9e2cd3cf76cff6
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-09-21 22:01:01 +0200

    Added task manager

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 Task= input("Enter your task: ")
 Priority = input("priority (high, medium, low): ").lower()
-Time_Bound = input("Is the task time-bound? (yes or no): ").lower()
+TimeBound = input("Is the task time-bound? (yes or no): ").lower()
 
 # Generate reminder based on priority and time sensitivity
 match Priority:
@@ -14,7 +14,7 @@ match Priority:
         reminder = "priority not recognized."
 
 # Modify reminder if task is time-bound
-if Time_Bound == "yes":
+if TimeBound == "yes":
     reminder += " It requires immediate attention today!"
 
 # Print the customized reminder
