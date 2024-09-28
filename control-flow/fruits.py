9b48ae8979e20c814c5764710d3b9551b8bee5f8
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-09-28 23:30:08 +0200

    arithmetic operations and main files

---
+++ control-flow/fruits.py
@@ -0,0 +1,3 @@
+my_list = "[apple; strawberry; cherry; berry; orange]"
+
+print (my_list[1])
--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,20 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+    
+    # Check if the operation is valid
+    if operation not in ['add', 'subtract', 'multiply', 'divide']:
+        raise ValueError("Invalid operation. Must be 'add', 'subtract', 'multiply', or 'divide'.")
+
+    # Perform the operation
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        # Check for division by zero
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        else:
+            return num1 / num2
 
