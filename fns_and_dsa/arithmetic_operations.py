bef029f8a726e896e1587c8bdb12643505d9ea99
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-09-28 23:37:52 +0200

    modified files

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float:
+def perform_operation(num1, num2, operation) -> float:
     
     # Check if the operation is valid
     if operation not in ['add', 'subtract', 'multiply', 'divide']:
