75e7e71010a004158cca8765f300a657153cff86
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-09-29 01:40:31 +0200

    added the datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,7 @@ def display_current_datetime():
     print(f"The current Date and Time is: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
 def calculate_future_date():
-    number_of_days = int(input("Enter the number of days: "))
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.datetime.now()
     future_date = current_date + datetime.timedelta(days=number_of_days)
     print(f"Future Date: {future_date.strftime('%Y-%m-%d')}")
