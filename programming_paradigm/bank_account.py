93a926b491707c27d0c187440ec23d99524b9d1b
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-10-03 19:56:56 +0200

    update bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,8 +9,11 @@ class BankAccount:
     def withdraw(self, amount):
         if 0 < amount <= self._account_balance:
              amount = self._account_balance -amount
-        return True
-        return False
+             print("Withdrawn: ${amount}")
+        else:
+            print("Insufficient funds")
+        
+    
     
     def display_balance(self):
         print(f"Current Balance: ${self._account_balance:.2f}")
