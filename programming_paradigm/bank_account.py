2506dbb80edf634007b93074888c4fdccf1d9a93
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-10-06 18:50:51 +0200

    updated bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+        self.account_balance = float(initial_balance)
 
     def deposit(self, amount):
         self.account_balance += amount
@@ -12,4 +12,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(float(f"Current Balance: ${self.account_balance}"))
+        print(f"Current Balance: ${self.account_balance:.2f}")
