501138e79932e1a8c6456d9629bb62a05fca172f
Kokeletso Molefe <k.molefe@lab.clicklearning.org>
2024-10-13 20:33:30 +0200

    updated the library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,8 +8,7 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title)
-        super().__init__(author)
+        super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
@@ -17,8 +16,7 @@ class EBook(Book):
        
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title)
-        super().__init__(author)
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
